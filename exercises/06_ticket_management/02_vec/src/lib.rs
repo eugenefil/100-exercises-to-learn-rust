2e6d2abbbd7586f633cf7bb175be60ca39f3bce1
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-06-13 19:59:14 +0200

    Done 06_ticket_management 00-05

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,11 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fibs = vec![0, 1];
+    for i in 2..=(n as usize) {
+        fibs.push(fibs[i - 2] + fibs[i - 1]);
+    }
+    fibs[n as usize]
 }
 
 #[cfg(test)]
