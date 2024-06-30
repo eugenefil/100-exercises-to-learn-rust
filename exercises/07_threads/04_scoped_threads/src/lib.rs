8c0520b47442f7009fc0250699a2bac4b3aa21e2
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-06-30 13:13:59 +0200

    Done 07_threads 04

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -3,7 +3,12 @@
 //  Don't perform any heap allocation. Don't leak any memory.
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+    std::thread::scope(|s| {
+        let left = s.spawn(|| v[..mid].iter().sum::<i32>());
+        let right = s.spawn(|| v[mid..].iter().sum::<i32>());
+        left.join().unwrap() + right.join().unwrap()
+    })
 }
 
 #[cfg(test)]
