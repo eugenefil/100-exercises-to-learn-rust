45731f3a00ea5be3e3e8b218a364d8b5ec75287d
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-07-21 20:22:43 +0200

    Done 07_threads 10

--- exercises/07_threads/10_patch/src/lib.rs
+++ exercises/07_threads/10_patch/src/lib.rs
@@ -35,7 +35,16 @@ impl TicketStoreClient {
         Ok(response_receiver.recv().unwrap())
     }
 
-    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {}
+    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {
+        let (response_sender, response_receiver) = sync_channel(1);
+        self.sender
+            .try_send(Command::Update {
+                patch: ticket_patch,
+                response_channel: response_sender,
+            })
+            .map_err(|_| OverloadedError)?;
+        Ok(response_receiver.recv().unwrap())
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -85,7 +94,18 @@ pub fn server(receiver: Receiver<Command>) {
                 patch,
                 response_channel,
             }) => {
-                todo!()
+                let ticket = store.get_mut(patch.id).unwrap();
+                let TicketPatch { title, description, status, .. } = patch;
+                if let Some(title) = title {
+                    ticket.title = title;
+                }
+                if let Some(description) = description {
+                    ticket.description = description;
+                }
+                if let Some(status) = status {
+                    ticket.status = status;
+                }
+                response_channel.send(()).unwrap();
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
